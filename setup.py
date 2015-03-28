e7b5e7295693448dc78ccba9e0ec815160877a4a
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2015-03-28 13:50:25 +0900

    Version 1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_file(name):
 
 setup(
     name='hyou',
-    version='1.1',
+    version='1.2',
     author='Shuhei Takahashi',
     author_email='nya@google.com',
     description='Pythonic Interface to manipulate Google Spreadsheet',
