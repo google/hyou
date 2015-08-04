3989ac86138a1399d1ab05771d5fdd1a903ef64d
Shuhei Takahashi <nya@google.com>
2015-08-04 17:26:41 +0900

    Bump version to 1.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_file(name):
 
 setup(
     name='hyou',
-    version='1.4',
+    version='1.5',
     author='Shuhei Takahashi',
     author_email='nya@google.com',
     description='Pythonic Interface to access Google Spreadsheet',
